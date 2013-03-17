41fccbed448d52e27acb512f0eedf80ccb6c8b26
Claudius Coenen <coenen@meso.net>
2013-03-17 16:29:13 +0100

    restricting frontend to viewing wishes for now

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Wishlist::Application.routes.draw do
-  resources :wishes
+  resources :wishes, :only => [:index, :show] do
+    get 'take', :on => :member
+  end
 
   root :to => 'wishes#index'
 end
--- test/functional/wishes_controller_test.rb
+++ test/functional/wishes_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class WishesControllerTest < ActionController::TestCase
   setup do
-    @wish = wishes(:one)
+    @teapot = wishes(:teapot)
   end
 
   test "should get index" do
@@ -11,39 +11,9 @@ class WishesControllerTest < ActionController::TestCase
     assert_not_nil assigns(:wishes)
   end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create wish" do
-    assert_difference('Wish.count') do
-      post :create, wish: { notes: @wish.notes, secret: @wish.secret, taken_by: @wish.taken_by, title: @wish.title, url: @wish.url }
-    end
-
-    assert_redirected_to wish_path(assigns(:wish))
-  end
-
   test "should show wish" do
-    get :show, id: @wish
+    get :show, id: @teapot
     assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @wish
-    assert_response :success
-  end
-
-  test "should update wish" do
-    put :update, id: @wish, wish: { notes: @wish.notes, secret: @wish.secret, taken_by: @wish.taken_by, title: @wish.title, url: @wish.url }
-    assert_redirected_to wish_path(assigns(:wish))
-  end
-
-  test "should destroy wish" do
-    assert_difference('Wish.count', -1) do
-      delete :destroy, id: @wish
-    end
-
-    assert_redirected_to wishes_path
+    assert_equal(assigns(:wish).title, @teapot.title)
   end
 end
